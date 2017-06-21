0de7dd8f0045832f92138c165dc4840843df1218
Adi Fatol <adrian.sergiu@gmail.com>
2017-06-21 22:36:09 +0300

    First miniphlow commit  - composer dependencies  - Node.php abstract class  - gitignore for vendor folder
    Signed-off-by: Adi Fatol <adrian.sergiu@gmail.com>

---
+++ Nodes/Node.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Nodes;
+
+/* Base class for nodes in the network */
+
+abstract class Node {
+
+    /* A list of nodes with edges into this node */
+    var $inbound_nodes;
+
+    /*  The eventual value of this node. Set by running
+        the forward() method */
+    var $value;
+
+    /*  Set by running the backward() method */
+    var $gradients;
+
+    /* A list of nodes that this node outputs to */
+    var $outbound_nodes;
+
+    public function construct(array $inbound_nodes) {
+        $this->inbound_nodes = $inbound_nodes;
+        $this->value = null;
+        $this->outbound_nodes = [];
+
+        # Sets this node as an outbound node for all of
+        # this node's inputs.
+        foreach ($inbound_nodes as $node) {
+            $node->outbound_nodes[] = $this;
+        }
+    }
+
+    abstract function forward();
+
+    abstract function backward();
+}
--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
 
-echo "Hello World!"
+// instantiate Aura PSR-4 autoloader
+$loader = new \Aura\Autoload\Loader;
 
+// append to the SPL autoloader stack; use register(true) to prepend instead
+$loader->register();
+
+$loader->addPrefix('Nodes', '/projects/Miniphlow/Nodes');
+
+$node = new \Nodes\Node;
 ?>
